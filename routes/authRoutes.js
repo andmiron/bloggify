9bfd11028d68bf70d8057741dc2e9d0e305ec912
Andrii <sylneyshii@gmail.com>
2023-04-23 02:56:03 +0300

    little work

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController')
 router.route('/signup')
     .get((req,res,next) => {
         res.render('signup')
+        next()
     })
     .post(authController.signup)
 
